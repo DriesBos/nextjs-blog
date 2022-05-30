f8af2f174ff2beff2e48ba6d6596c2fb7142a135
DriesBos <info@driesbos.com>
2022-05-30 14:16:44 +0900

    stle: added font styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import "../styles/reset.css";
+import "../styles/fonts.css";
 import "../styles/globals.sass";
+import "../styles/typography.sass";
 import Header from "../components/header/Header";
 
 function MyApp({ Component, pageProps }) {
