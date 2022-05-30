e245ea9d35f82bda15210d911424bd3565de3e36
DriesBos <info@driesbos.com>
2022-05-30 16:54:45 +0900

    style: essays page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -25,42 +25,55 @@ function AllPostsPage(props) {
   return (
     <div>
       <div className="sectionIntro">
-        <h1>Original Essays</h1>
-        <p>
-          The PRACTICAL IDEALIST is dedicated to supporting and amplifying
-          innovation – both technological and intellectual – which, as our name
-          declares, combines the practical and the idealistic. We publish pieces
-          by a variety of thinkers on a variety topics, freely traversing styles
-          and perspectives. No matter the style, discipline, or subject matter,
-          we value systems thinking, actionable idealism, and beauty—because
-          without beauty, it’s all for nought. We welcome submissions from poets
-          and philosophers, artists and activists, academics and amateurs alike.
-          Want to contribute something? Click here.
-        </p>
+        <div>
+          <h1>Original Essays</h1>
+        </div>
+        <div>
+          <p>
+            The PRACTICAL IDEALIST is dedicated to supporting and amplifying
+            innovation – both technological and intellectual – which, as our
+            name declares, combines the practical and the idealistic. We publish
+            pieces by a variety of thinkers on a variety topics, freely
+            traversing styles and perspectives. No matter the style, discipline,
+            or subject matter, we value systems thinking, actionable idealism,
+            and beauty—because without beauty, it’s all for nought. We welcome
+            submissions from poets and philosophers, artists and activists,
+            academics and amateurs alike. Want to contribute something? Click
+            here.
+          </p>
+        </div>
       </div>
       <br></br>
       <ul className="articleList">
         {props.posts.map((post) => (
-          <li className="link" key={post.id}>
-            <Link
-              href={{
-                pathname: "/posts/[slug]",
-                query: { slug: post.slug },
-              }}
-            >
+          <li className="articleList-Item" key={post.id}>
+            <div className="articleList-Image">
+              <img src={post.feature_image} />
+            </div>
+
+            <div className="articleList-Content">
+              <Link
+                href={{
+                  pathname: "/posts/[slug]",
+                  query: { slug: post.slug },
+                }}
+              >
+                <div>
+                  <p>{post.title}</p>
+                </div>
+              </Link>
               <div>
-                <p>{post.title}</p>
+                <ul className="authorList">
+                  {post.authors.map((author) => (
+                    <li key={author.id}>
+                      <p>{author.name}</p>
+                    </li>
+                  ))}
+                </ul>
               </div>
-            </Link>
-            <ul className="authorList">
-              {post.authors.map((author) => (
-                <li key={author.id}>
-                  <p>{author.name}</p>
-                </li>
-              ))}
-            </ul>
-            <div>
-              <p>{post.excerpt}</p>
+              <div>
+                <p>{post.excerpt}</p>
+              </div>
             </div>
           </li>
         ))}
